c3d33a69f4685908051d6a943aadb38726a5c213
Fílip Anselmo <filip.anselmo@jointecnologia.com.br>
2022-02-03 14:39:48 -0400

    Grid

---
+++ grid-1/src/components/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav>
+    <v-toolbar>
+      <v-app-bar-nav-icon
+        class="grey--text"
+        @click="drawer != drawer"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Todo</span>
+        <span>Ninja</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn color="grey">
+        <span>Sign Out</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-navigation-drawer absolute temporary v-model="drawer" class="indigo">
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon class="white--text">dashboard</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-tile class="white--text">Dashboard</v-list-tile-tile>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data: () => ({
+    drawer: false,
+    links: [
+       {icon: 'dashboard', text: 'Dashboard', route: '/'},
+       {icon: 'folder', text: 'Projects', route: '/projects'},
+       {icon: 'person', text: 'Team', route: '/team'}, 
+    ]
+  }),
+};
+</script>
+
+<style></style>
---
+++ grid-1/src/views/Grid.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="grid">
+    <v-container class="my-5">
+      <v-card class="pa-3">
+        <v-layout row wrap>
+          <v-flex xs12 md6>
+            <div class="caption grey--text">Projetct Title</div>
+            <div>Create a new website</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Person</div>
+            <div>Net Ninja</div>
+          </v-flex>
+           <v-flex xs6 sm4 md2>
+               <div class="caption grey--text">Stattus</div>
+               <div>ongoing</div>
+          </v-flex>
+        </v-layout>
+      </v-card>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
