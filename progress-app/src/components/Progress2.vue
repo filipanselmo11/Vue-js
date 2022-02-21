b4d62ea9f6e4b4be386d0c77e0f1f43876492732
Fílip Anselmo <filip.anselmo@jointecnologia.com.br>
2022-02-21 08:30:52 -0400

    Custom Colors

---
+++ progress-app/src/components/Progress2.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-center">
+    <v-progress-linear
+      background-color="pink ligthen-3"
+      color="pink ligthen-1"
+      value="15"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Progress2",
+  data: () => ({}),
+};
+</script>
+
+<style></style>
