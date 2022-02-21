8bdeb24ddef00443f65d9302d71e229fadd83524
Fílip Anselmo <filip.anselmo@jointecnologia.com.br>
2022-02-21 16:06:51 -0400

    Progress Bar Component

---
+++ progress-app/src/components/ProgressBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <v-progress-linear height="20" v-model="progress"></v-progress-linear>
+    <br />
+    <v-btn @click="updateValue">
+      <v-icon>mdi-play</v-icon>
+    </v-btn>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    progress: 0,
+    value: '',
+  }),
+
+  methods: {
+    updateValue() {
+      this.value = this.progress++;
+    },
+  },
+};
+</script>
+<style></style>
