b618a411522db47251e05ac43383b7175b553d0b
Fílip Anselmo <filip.anselmo@jointecnologia.com.br>
2022-03-09 08:51:07 -0400

    Adicionando transição e lazy src

--- vue-image-slider/src/App.vue
+++ vue-image-slider/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <v-app>
     <v-main>
-     <new-app-image></new-app-image>
+      <new-image-component></new-image-component>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import NewAppImage from './components/NewAppImage.vue';
+//import NewAppImage from './components/NewAppImage.vue';
 //import SomethingWayComponent from './components/SomethingWayComponent.vue';
 //import OtherImageComponent from './components/OtherImageComponent.vue';
-//import NewImageComponent from './components/NewImageComponent.vue'
+import NewImageComponent from './components/NewImageComponent.vue'
 //import ImageComponent from './components/ImageComponent.vue';
 //import ImageSliderComponent from "./components/ImageSliderComponent.vue";
 
@@ -20,8 +20,8 @@ export default {
   components: {
     //OtherImageComponent,
     //SomethingWayComponen,
-    NewAppImage,
-    //NewImageComponent
+    //NewAppImage,
+    NewImageComponent
     //ImageComponent
     //ImageSliderComponent,
   },
