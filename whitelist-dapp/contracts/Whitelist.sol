7851e5877db30767e7ebe6d28ecc32bf07277f3b
FilipAnselmo <filip.anselmo@jointecnologia.com.br>
2022-03-31 00:41:16 -0400

    Subindo novos projetos

---
+++ whitelist-dapp/contracts/Whitelist.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Whitelist {
+    address owner; // variável que conterá o endereço do implementador do contrato
+
+    constructor(){
+        owner = msg.sender; // definindo o proprietário do implementador do contrato
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Proprietário: o chamador não é o proprietário");
+        _;
+    }
+
+    modifier isWhitelisted(address _address){//Modificador pra verificar se o endereço consta na lista branca
+        require(whitelistedAddresses[_address], "Você precisa estar na lista de permissões");
+        _;
+    }
+
+    mapping(address => bool) whitelistedAddresses; //lista de endereços, para armazenar os mesmos, fazer um mapeamento para verificar se o endereço consta na lista
+
+    function addUser(address _addresstoWhitelist) public onlyOwner {// Função para adicionar um novo endereço na lista, mudando seu status pra true
+        whitelistedAddresses[_addresstoWhitelist] = true; //true = permitido //false = não permitido  
+    }
+
+    function exampleFunction() public view isWhitelisted(msg.sender) returns (bool) {
+        return true;
+    }
+
+    function verifyUser(address _whitelistedAddress) public view returns(bool){//Função que verifica se o endereço consta na lista branca
+        bool userIsWhitelisted = whitelistedAddresses[_whitelistedAddress];
+        return userIsWhitelisted;
+    }
+
+
+}
