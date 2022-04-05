0fa253c1cba38d66a0617167b3bc70343494092c
Filip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2022-04-05 14:29:25 -0400

    Adicionando novas coisas

---
+++ date-app/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <date-component></date-component>
+  </div>
+</template>
+
+<script>
+import DateComponent from './components/DateComponent.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    DateComponent
+   
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ date-app/src/components/DateComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <!--<h1> Array antes da função de sort</h1>
+    {{ this.activities }}
+    <h1>Array depois da função de Sort</h1>
+    {{this.sort(this.activities)}}-->
+  </div>
+</template>
+
+<script>
+const activities = [
+  { title: "Hiking", date: new Date("2019-06-28") },
+  { title: "Shopping", date: new Date("2019-06-10") },
+  { title: "Trekking", date: new Date("2019-06-22") },
+];
+console.log("Array Activities ", activities);
+//const sortedActivities = activities.sort((a, b) => b.date - a.date);
+const sortedActivities = activities.slice().sort((a, b) => b.date - a.date);
+console.log("Array Activities Sorted ", sortedActivities);
+export default {
+  data: () => ({
+    activities: [
+      { title: "Hiking", date: new Date("2019-06-28") },
+      { title: "Shopping", date: new Date("2019-06-10") },
+      { title: "Trekking", date: new Date("2019-06-22") },
+    ],
+    sortedActivities: [],
+  }),
+
+  methods: {
+    sort(activities) {
+      let sort = activities.sort((a, b) => b.date - a.date);
+      this.sortedActivities = sort;
+      return this.sortedActivities;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
