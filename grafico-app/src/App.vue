de07cf2a8fa72e753357668cb11b38dfecfeebd0
Filip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2022-04-05 15:29:48 -0400

    Subindo outro chart component

--- grafico-app/src/App.vue
+++ grafico-app/src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <multiple-series></multiple-series>
+  <chart-component-1></chart-component-1>
 </template>
 
 <script>
+import ChartComponent1 from './components/ChartComponent1.vue';
+
 /*eslint-disabled*/
 //import IconChartComponent1 from "./components/IconChartComponent1.vue";
-import IconChartComponent2 from "./components/IconChartComponent2.vue";
-import MultipleSeries from './components/MultipleSeries.vue';
+//import IconChartComponent2 from "./components/IconChartComponent2.vue";
+//import MultipleSeries from './components/MultipleSeries.vue';
 //import FNomeComponent from './components/FNomeComponent.vue'
 export default {
-  components: { IconChartComponent2, MultipleSeries },
+  components: {ChartComponent1},
 };
 </script>
 
