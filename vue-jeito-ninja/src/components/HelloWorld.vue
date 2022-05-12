10a8dc93a112268517918b6ddffc22814e71f117
Your Name <you@example.com>
2022-05-12 17:29:06 -0400

    v-on, manipulando eventos

---
+++ vue-jeito-ninja/src/components/HelloWorld.vue
@@ -0,0 +1,114 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <form action="https://google.com">
+      <button @click.prevent.stop="enviar">Enviar</button>
+    </form>
+    <!-- <button :class="btnLimpar" :style="btnStyleLimpar">LIMPar</button>
+    <button :class="btnEnviar">ENVIAR</button> -->
+    <!-- <img :src="imagem" :alt="alt" />
+    <p :class="classHtml">texto</p> -->
+    <!-- <label for="nome">Nome</label>
+    <input type="text" v-model="nome" />
+    <br />
+    <label for="">Telefone</label>
+    <input type="text" v-model="telefone" />
+    <br />
+    <label for="">Deseja receber nossas novidades</label>
+    <input type="radio" value="Sim" v-model="novidades" /> Sim
+    <input type="radio" value="Não" v-model="novidades" /> Não
+    <br />
+    <label for="">Interesses</label>
+    <input type="checkbox" value="Futebol" v-model="interesses" /> Futebol <br />
+    <input type="checkbox" value="Formula 1" v-model="interesses" /> Formula 1 <br />
+    <br />
+
+    <label for="">Como nos conheceu ?</label>
+    <select v-model="conheceu">
+      <option value="">Escolha</option>
+      <option value="Google">Google</option>
+      <option value="TV">TV</option>
+      <option value="Revista">Revista</option>
+    </select>
+    <br />
+
+    <button>ENVIAR</button>
+
+    <hr />
+
+    Nome: {{ nome }} <br />
+    Telefone: {{ telefone }} <br />
+    Novidades: {{ novidades }} <br />
+    Interesses: {{ interesses }} <br />
+    Como nos conheceu ? {{ conheceu }} <br /> -->
+    <!-- <div v-if="usuario.id == 1">
+      Id: {{ usuario.id }} Nome: {{ usuario.nome }}<br />
+      Profissao: {{ usuario.profissao }} <br />
+    </div>
+    <div v-else>
+      Nenhum usuario com o id = 1
+    </div> -->
+    <!-- <ul>
+      <li v-for="(index, item) in linguagens" :key="index">
+        {{ index }} -
+        {{ item.nome }}
+      </li>
+    </ul> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data: () => ({
+    titulo: "Vue Js do Jeito Ninja",
+    //texto: false, //texto: true
+    usuario: {
+      id: 1,
+      nome: "Yoshimitsu",
+      profissao: "Dev",
+    },
+    linguagens: [
+      { nome: "Javascript" },
+      { nome: "PHP" },
+      { nome: "C#" },
+      { nome: "JavaZao" },
+      { nome: "Python" },
+    ],
+    nome: "",
+    telefone: "",
+    novidades: "",
+    interesses: [],
+    conheceu: "",
+    imagem:
+      "https://c4.wallpaperflare.com/wallpaper/371/29/254/superhero-dc-comics-red-hood-yellow-background-wallpaper-preview.jpg",
+    alt: "Red Hood",
+    classHtml: "green",
+    btnLimpar: {
+      "btn-danger": true,
+      "btn-sm": false,
+    },
+    btnEnviar: ["btn-primary", { "btn-lg": true }],
+    btnStyleLimpar: [
+      { "font-size": "50px" },
+      { "font-weight": "bold" },
+      { "text-transform": "lowercase" },
+    ],
+  }),
+  methods: {
+    enviar() {
+      alert("Enviado");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.red {
+  color: red;
+}
+
+.green {
+  color: green;
+}
+</style>
