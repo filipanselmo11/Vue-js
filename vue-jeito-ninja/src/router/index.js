91c018665be7ee7c07e6c739380392a5e83e25b6
Your Name <you@example.com>
2022-05-15 22:14:34 -0400

    redirecionamento e rotas padrão

--- vue-jeito-ninja/src/router/index.js
+++ vue-jeito-ninja/src/router/index.js
@@ -4,7 +4,7 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 const routes = [
-    { path: '/', name: 'home', redrect: '' },
+    { path: '/', name: 'home', redrect: { name: 'transformers-content', params: { name: 'optimus-prime' } } },
     {
         path: '/transformers',
         name: 'transformers',
