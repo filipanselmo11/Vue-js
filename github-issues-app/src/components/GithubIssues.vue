6f3f27d23c7ba66e2a9cc88d1ee51f5d3b6aceb8
Your Name <you@example.com>
2022-05-29 18:40:24 -0400

    Add tabela e funções

---
+++ github-issues-app/src/components/GithubIssues.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container>
+    <h1>Vue JS + Vuetify + Github</h1>
+    <p>Página que lista issues de um repositório do Github, utilizando Vue js.</p>
+    <v-row>
+      <v-col>
+        <v-form ref="form" lazy-validation>
+          <v-text-field v-model="name" label="Github UserName" required></v-text-field>
+          <v-text-field v-model="repo" label="Github Repo" required></v-text-field>
+          <v-btn @click.prevent.stop="getIssues" color="success" class="mr-4">Go</v-btn>
+          <v-btn @click.prevent.stop="reset" color="error" class="mr-4">Limpar</v-btn>
+        </v-form>
+      </v-col>
+    </v-row>
+
+    <br />
+    <hr />
+    <br />
+
+    <v-data-table
+      dense
+      :headers="headers"
+      item-key="name"
+      class="elevation-1"
+    ></v-data-table>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "GithubIssues",
+  data: () => ({
+    name: undefined,
+    repo: undefined,
+    headers: [{ text: "Número" }, { text: "Título" }],
+  }),
+  methods: {
+    reset() {
+      this.name = "";
+      this.repo = "";
+      console.log("Resetados com Sucesso");
+    },
+    getIssues() {
+      console.log("FUNFANDO");
+    },
+  },
+};
+</script>
+
+<style></style>
--- github-issues-app/src/plugins/vuetify.js
+++ github-issues-app/src/plugins/vuetify.js
@@ -4,4 +4,18 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: { dark: true },
+    // theme: {
+    //     themes: {
+    //         dark: {
+    //             primary: '#3f51b5',
+    //             secondary: '#4caf50',
+    //             accent: '#009688',
+    //             error: '#00bcd4',
+    //             warning: '#8bc34a',
+    //             info: '#ffeb3b',
+    //             success: '#ffc107'
+    //         }
+    //     }
+    // }
 });
