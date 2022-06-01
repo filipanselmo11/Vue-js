d346c206b2954c2798bed66819cc18823da696eb
Your Name <you@example.com>
2022-06-01 10:56:57 -0400

    Marvel DC APP

---
+++ marvel-dc-app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <div>
+      <v-toolbar dark prominent src="./assets/Batman-pantera-negra.png">
+        <v-toolbar-title>DC Marvel APP</v-toolbar-title>
+      </v-toolbar>
+    </div>
+    <br/>
+    <br/>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ marvel-dc-app/src/views/MarvelPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="marvelpage">
+    <h1>Marvel</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
